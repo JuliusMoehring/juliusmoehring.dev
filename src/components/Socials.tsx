ea1b235499d0fbe79ab119e271ac622ce372e89c
JuliusMoehring <44407680+JuliusMoehring@users.noreply.github.com>
2023-04-06 16:56:58 +0200

    Fix favicon | update github actions

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -4,32 +4,32 @@ import { DataCampIcon, GitHubIcon, LinkedInIcon, TwitterIcon } from './icons/Soc
 import { SocialLink } from './UI/SocialLink';
 
 const socials = [
-  {
-    href: 'https://twitter.com/julius_mohring',
-    'aria-label': 'Follow on Twitter',
-    icon: TwitterIcon,
-  },
-  {
-    href: 'https://www.datacamp.com/profile/JuliusMoehring',
-    'aria-label': 'Check my DataCamp profile',
-    icon: DataCampIcon,
-  },
-  {
-    href: 'https://github.com/JuliusMoehring',
-    'aria-label': 'Follow on GitHub',
-    icon: GitHubIcon,
-  },
-  {
-    href: 'https://www.linkedin.com/in/karl-julius-m%C3%B6hring-583b99172/',
-    'aria-label': 'Follow on LinkedIn',
-    icon: LinkedInIcon,
-  },
+    {
+        href: 'https://twitter.com/julius_mohring',
+        'aria-label': 'Follow on Twitter',
+        icon: TwitterIcon,
+    },
+    {
+        href: 'https://www.datacamp.com/profile/JuliusMoehring',
+        'aria-label': 'Check my DataCamp profile',
+        icon: DataCampIcon,
+    },
+    {
+        href: 'https://github.com/JuliusMoehring',
+        'aria-label': 'Follow on GitHub',
+        icon: GitHubIcon,
+    },
+    {
+        href: 'https://www.linkedin.com/in/karl-julius-m%C3%B6hring-583b99172/',
+        'aria-label': 'Follow on LinkedIn',
+        icon: LinkedInIcon,
+    },
 ];
 
 export const Socials: Component = () => {
-  return (
-    <div class="flex gap-6">
-      <For each={socials}>{({ icon, ...rest }) => <SocialLink icon={icon} {...rest} />}</For>
-    </div>
-  );
+    return (
+        <div class="flex gap-6">
+            <For each={socials}>{({ icon, ...rest }) => <SocialLink icon={icon} {...rest} />}</For>
+        </div>
+    );
 };
